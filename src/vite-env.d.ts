82b26ad66ae120e8420832750afae54c356761d4
LuanAssis01 <assisluan863@gmail.com>
2025-12-07 18:17:17 -0300

    feat: add url backend in production

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,6 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string;
-  // adicione outras variáveis de ambiente aqui se necessário
 }
 
 interface ImportMeta {
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3333',
+        target: 'http://193.203.183.147:3333',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
